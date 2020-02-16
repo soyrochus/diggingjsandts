7204dcdbb1f3afd09b2c6e2bef9a3553ce0a4554
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2020-02-16 22:50:55 +0100

    wip walker

--- src/scratchpad.js
+++ src/scratchpad.js
@@ -1,31 +1,10 @@
-//compiles fine
-function functionscope(){
+//import {readFile} from 'fs'
+const {readFile} = require('fs')
 
-    for(i=0;i < 10; i++){
-  
-      console.log(i)
+readFile("app/ironman.txt", 'utf8', (error, data)=> {
+    if (error) throw error
+
+    for(let s of data.split('\n')){
+        console.log(s)
     }
-    if (true){ 
-      var i = 10000
-    } 
-    console.log(i)
-  }
-  
-  //gives error TS2304: Cannot find name 'i'.
-  function blockscope(){
-  
-    //let i;
-    
-    for(i=0;i < 10; i++){
-  
-      console.log(i)
-    }
-    if (true){ 
-      let i = 100000
-    }
-    console.log(i)
-  }
-  
-  functionscope()
-  blockscope()
-  
+})
--- src/scratchpad.ts
+++ src/scratchpad.ts
@@ -1,9 +1,33 @@
+
+import {promisify} from 'util'
 import {readFile} from 'fs'
+//const util = require('util');
 
-readFile("app/ironman.txt", 'utf8', (error: any, data: string)=> {
-    if (error) throw error
+let prnerror = (error:any)=> {
+    console.log(error)
+}
+console.log("you see me first")
+let readFp = promisify(readFile)
 
+readFp("app/ironman.txt", 'utf8').then((data)=> {
+   
+   console.log("you see me last")
+   for(let s of data.split('\n')){
+       console.log(s)
+   }
+}).catch(prnerror)
+
+console.log("you see me second")
+
+console.log("----------------------------------")
+
+console.log("you see me first")
+async function printIronMan() {
+    let data = await readFp("app/ironman.txt", 'utf8')
+    console.log("you see me last")
     for(let s of data.split('\n')){
         console.log(s)
     }
-})
+}
+printIronMan()
+console.log("you see me second")
