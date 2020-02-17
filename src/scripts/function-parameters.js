32a3985135f1b0f92196e760791a3db65099450e
Iwan van der Kleijn <ivanderk@capgemini.com>
2020-02-17 11:23:58 +0100

    fixes & powerpoint

---
+++ src/scripts/function-parameters.js
@@ -0,0 +1,21 @@
+
+let buildName = (firstName, lastName, title= 'Mr./Ms.') =>{
+    if (lastName)
+      return `${title} ${firstName} ${lastName}`
+    else
+      return  `${firstName}`
+}
+   
+// Note that this will not work in TypeScript with strict options set. It will fail to compile with 
+// error TS2554: Expected 2-3 arguments, but got 1.
+console.log(buildName("John"))  // -> ‘John’
+console.log(buildName("Rupert", "Grint")) // -> ‘Mr./Ms. Rupert Grint’
+console.log(buildName("Emma", "Watson", "Miss")) // -> ‘Miss Emma Watson’
+   
+   
+//The rest parameter syntax allows us to represent an indefinite number of arguments as an array.
+
+let cast = (...castmembers) =>{
+    console.log(`Harry Potter cast members are: ${castmembers} `)
+}
+cast("Daniel Radcliffe", "Emma Watson", "Rupter Grint", "Alan Rickman", "Michael Gambon")
---
+++ src/scripts/function-parameters.ts
@@ -0,0 +1,10 @@
+let buildName = (firstName: string, lastName?: string, title: string = 'Mr./Ms.') =>{
+    if (lastName)
+      return `${title} ${firstName} ${lastName}`
+    else
+      return  `${firstName}`
+}
+   
+console.log(buildName("John"))  // -> ‘John’
+console.log(buildName("Rupert", "Grint")) // -> ‘Mr./Ms. Rupert Grint’
+console.log(buildName("Emma", "Watson", "Miss")) // -> ‘Miss Emma Watson’
