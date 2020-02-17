4363988a9de932e410542e88abfb1d3670368ea5
Iwan van der Kleijn <ivanderk@capgemini.com>
2020-02-17 15:20:54 +0100

    Cleanup

--- src/scripts/contextual.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-fs_1.readFile('d:/tmp/data.txt', (err, buf) => {
-    console.log(buf.toString());
-});
-//# sourceMappingURL=contextual.js.map
