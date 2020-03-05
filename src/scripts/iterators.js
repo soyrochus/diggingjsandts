da8c17f3525f738346a6a67f73658e1510c53eb0
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2020-03-05 20:51:30 +0100

    Fixed iterators.js & added 'take' imple

--- src/scripts/iterators.js
+++ src/scripts/iterators.js
@@ -14,14 +14,27 @@ function fibonacci_iterator() {
     }
 }
 
-let fibonaci_iter = num => {
+let fibonaci_iter = (num) => {
     let iterator = fibonacci_iterator()
     let fib = []
-    for (let i = 0; i <= 20; i++) {
+    for (let i = 0; i <= num; i++) {
         let { value, done } = iterator.next()
         fib.push(value)
     }
     return fib
 }
 
+let take = (iterator, num) => {
+    let data = []
+    let it = iterator()
+    for (let i = 0; i <= num; i++) {
+        let { value, done } = it.next()
+        if (done) return data
+        data.push(value)
+    }
+    return data
+} 
+
 console.log(fibonaci_iter(20)) //  // -> [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765]
+
+console.log(take(fibonacci_iterator, 20)) //  // -> [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765]
